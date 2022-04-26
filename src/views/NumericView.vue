be7f9ff80d3174f96eabd4871b2937381dd63bda
ammarh <ammar.hatiya@gmail.com>
2022-04-26 19:51:35 -0400

    Finished lesson 2, added styling and quiz

--- src/views/NumericView.vue
+++ src/views/NumericView.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="datatypes">
+        <NumericSidebar/>
+
   </div>
 </template>
 
 <script>
 
 
+import NumericSidebar from '../components/NumericSidebar.vue'
 
 
 
 export default {
   name: 'NumericView',
   components:{
+    NumericSidebar
   }
 
 }
